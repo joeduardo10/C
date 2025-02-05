05e379083466dbfb79844abf211958d5e72c551d
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-02-05 09:14:27 -0300

    adiciona funções para o jogo da forca e corrige contagem de chutes

--- forca.c
+++ forca.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include "forca.h"
 
+
+
+// variáveis globais
 char palavrasecreta[20];
 char chutes[26];
-int tentativas = 0;
+int chutesdados = 0;
+
+// agora aqui começam as implementações das funções,
+// do jeito que você está acostumado.
+
 
 void abertura() {
     printf("/****************/\n");
@@ -16,13 +24,13 @@ void chuta() {
     printf("Qual letra? ");
     scanf(" %c", &chute);
 
-    chutes[tentativas] = chute;
+    chutes[chutesdados] = chute;
 }
 
 // Essa função não foi discutida no último vídeo
 int jachutou(char letra) {
     int achou = 0;
-    for(int j = 0; j < tentativas; j++) {
+    for(int j = 0; j < chutesdados; j++) {
         if(chutes[j] == letra) {
             achou = 1;
             break;
@@ -35,7 +43,7 @@ int jachutou(char letra) {
 // Essa função não foi discutida no último vídeo
 void desenhaforca() {
 
-    printf("Você já deu %d chutes\n", tentativas);
+    printf("Você já deu %d chutes\n", chutesdados);
 
     for(int i = 0; i < strlen(palavrasecreta); i++) {
 
@@ -54,12 +62,41 @@ void desenhaforca() {
 void escolhepalavra() {
     sprintf(palavrasecreta, "MELANCIA");
 }
+int enforcou() {
+
+    int erros = 0;
+
+    // vamos fazer o loop em todos os chutes dados
+    for(int i = 0; i < chutesdados; i++) {
+
+        int existe = 0;
+        // agora vamos olhar letra a letra da palavra secreta
+        // e ver se encontramos o chute aqui
+        for(int j = 0; j < strlen(palavrasecreta); j++) {
+            if(chutes[i] == palavrasecreta[j]) {
+                // encontramos, vamos quebrar o loop
+                existe = 1;
+                break;
+            }
+        }
+        // se nao encontrou, soma um na quantidade de erros
+        if(!existe) erros++;
+    }
+    // se tivermos mais do que 5 erros, retornamos 1
+    // caso contrario, retornamos 0.
+    return erros >= 5;
+}
+int acertou() {
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+        if(!jachutou(palavrasecreta[i])) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
 
 int main() {
-
-    int acertou = 0;
-    int enforcou = 0;
-
     abertura();
     escolhepalavra();
 
@@ -68,8 +105,8 @@ int main() {
         desenhaforca();
         chuta();
 
-        tentativas++;
+        chutesdados++;
 
-    } while (!acertou && !enforcou);
+    } while (!acertou() && !enforcou());
 
 }
---
+++ forca.h
@@ -0,0 +1,9 @@
+// lista de funções que aparecerão no arquivo
+
+int enforcou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+int acertou();
+void desenhaforca();
+void escolhepalavra();
