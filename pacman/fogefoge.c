6a3bb1492a10688ca02244421a431bf9dbe1c11b
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-02-18 17:00:30 -0300

    refatora gerenciamento de mapa no jogo Pacman, implementando estrutura MAPA e funções associadas

--- pacman/fogefoge.c
+++ pacman/fogefoge.c
@@ -1,84 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "fogefoge.h"
-//matriz de 5 x 10
-char** mapa;
-int linhas;
-int colunas;
+#include "mapa.h"
 
-void liberaMapa(){
-    for(int i =0; i<linhas; i++){
-        free(mapa[i]);
-    }
-    free(mapa);
-}
-void alocamapa(){
-    mapa = malloc(sizeof(char*)* linhas);
-    for(int i =0; i<linhas; i++){
-        mapa[i] = malloc(sizeof(char)* (colunas+1));
-    }
-}
-void lemapa(){
-    FILE* f;
-    f= fopen("mapa.txt", "r");
-    if(f == 0){
-        printf("Erro na leitura do mapa\n");
-        exit(1);
-    }
+MAPA m;
+POSICAO heroi;
 
-    fscanf(f,"%d %d", &linhas, &colunas);   
-    
-    alocamapa();
-    
-        for(int i =0; i<5; i++){
-            fscanf(f, "%s", mapa[i]);
-        }
-        fclose(f);
-}
-void imprimemapa() {
-    for(int i = 0; i < linhas; i++) {
-        printf("%s\n", mapa[i]);
-    }
-}
 int acabou(){
     return 0;
 }
-void move(char direcao){
-    int x;
-    int y;
-    // acha a posiçao do fogefoge
-    for(int i =0; i<linhas; i++){
-        for(int j =0; j<colunas; j++){
-            if(mapa[i][j] == '@'){
-                x = i;
-                y = j;
-                break;
-            }
-        }
-    }
-    switch (direcao){
-    case 'a':  //esquerda
-        mapa[x][y-1]= '@';
-        break;
-    case 'w': //cima
-    mapa[x-1][y]= '@';
-        break;
-    case 's': ///baixo
-        mapa[x+1][y]= '@';
-        break;
-    case 'd':   //direita
-        mapa[x][y+1]= '@';
-        break;
-    }
-    mapa[x][y] = '.';
+void move(char direcao) {
+
+	m.matriz[heroi.x][heroi.y] = '.';
+
+	switch(direcao) {
+		case 'a':
+			m.matriz[heroi.x][heroi.y-1] = '@';
+			heroi.y--;
+			break;
+		case 'w':
+			m.matriz[heroi.x-1][heroi.y] = '@';
+			heroi.x--;
+			break;
+		case 's':
+			m.matriz[heroi.x+1][heroi.y] = '@';
+			heroi.x++;
+			break;
+		case 'd':
+			m.matriz[heroi.x][heroi.y+1] = '@';
+			heroi.y++;
+			break;
+	}
+	
 }
 
  int main(){
-
-    lemapa();
+ 
+    lemapa(&m);
+    encontramapa(&m, &heroi, '@');
 
     do{
-        imprimemapa();
+        imprimemapa(&m);
         char comando;
         scanf(" %c", &comando);
         move(comando);
@@ -86,6 +48,6 @@ void move(char direcao){
     }while(!acabou());
 
 
-    liberaMapa();
+    liberamapa(&m);
               
     }
