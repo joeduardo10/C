ecc1ca8fab7bf52b801c6a44215d1b9763d1c8d8
José Eduardo <157645066+joeduardo10@users.noreply.github.com>
2025-04-15 22:57:07 -0300

    refatora lógica de movimento e validação no jogo Pacman, ajustando funções e estrutura do mapa

--- pacman/fogefoge.h
+++ pacman/fogefoge.h
@@ -1,12 +1,10 @@
-
 #define CIMA 'w'
 #define BAIXO 's'
-#define ESQUERDA 'a'
 #define DIREITA 'd'
+#define ESQUERDA 'a'
 
 int acabou();
 void move(char direcao);
+ 
 int ehdirecao(char direcao);
 void fantasmas();
-int praondefantasmavai(int xatual, int yatual, 
-    int* xdestino, int* ydestino);
